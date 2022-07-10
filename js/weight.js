5f65a4c5b446480a03ef40552456a8874a3ca959
Carol Waruingi <carolinewaruingu6@gmail.com>
2022-07-10 05:48:55 -0700

    This program converts weight from Pounds to Kilograms

--- js/weight.js
+++ js/weight.js
@@ -1,6 +1,6 @@
 function weightConverter(valNum){
-    var valNum = valNum*2.2046;
+    var valNum = valNum/2.2046;
     var n = valNum.toFixed(0);
-    document.getElementById("outputPounds").innerHTML=n;
+    document.getElementById("outputKilograms").innerHTML=n;
 
 }
